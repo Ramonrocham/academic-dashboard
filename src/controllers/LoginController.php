0e0d90a5594bd1ec49b4a744d29d14a86b000325
Ramon Rocha <anacletorrocham@gmail.com>
2026-04-07 10:41:34 -0300

    Feat: Conexão ao banco de dados

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -3,11 +3,14 @@
 namespace Ramon\Academic\controllers;
 
 use Ramon\Academic\core\Controller;
+use Ramon\Academic\core\Database;
 
 class LoginController extends Controller{
 
     public function index($msg = null){
-        dd(dotenv('DB_USER'));
+        $database = new Database();
+        $result = $database->query('SELECT 1 + 2 as soma');
+        dd($result->fetch()); // Testa a conexão
         $this->view('login/index', ['msg' => $msg]);
     }
 
--- src/core/Database.php
+++ src/core/Database.php
@@ -4,10 +4,42 @@ namespace Ramon\Academic\core;
 
 class Database{ 
 
-    public function connect(){
-        $host = $_ENV['DB_HOST'];
-        $dbname = $_ENV['DB_DB'];
-        $username = $_ENV['DB_USER'];
-        $password = $_ENV['DB_PASSWORD'];
+    private $connection = null;
+
+    private function connect(){
+        $host = dotenv('DB_HOST');
+        $dbname = dotenv('DB_NAME');
+        $username = dotenv('DB_USER');
+        $password = dotenv('DB_PASSWORD');
+        $driver = dotenv('DB_DRIVER');
+        $port = dotenv('DB_PORT');
+
+        $dsn = "$driver:host=db;port=$port;dbname=$dbname;";
+
+        $options = [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+        ];
+        try{
+            $this->connection = new \PDO($dsn, $username, $password, $options);
+        }catch(\PDOException $e){
+            dd($e->getMessage());
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function query($sql, $params = []){
+        if($this->connection === null){
+            $this->connect();
+        }
+        
+        try{
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        }catch(\PDOException $e){
+            dd($e->getMessage());
+            throw new \PDOException('Erro de consulta: ' . $e->getMessage(), (int)$e->getCode());
+        }
     }
 }
